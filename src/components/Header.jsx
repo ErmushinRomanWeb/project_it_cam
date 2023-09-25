95088f3eafd56eb82a7455839bb3c7102fae8878
ErmushinRoman <ermushinr@gmail.com>
2023-09-26 01:23:25 +0300

    did_first_css_module

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import './Header.css';
+import classes from './Header.module.css';// эта формулировка означает, что мы импортируем classes, который является объктом 
 
 const Header = () => {
     return (
-        <header className="header">
+        <header className={classes.header}> {/*помню, что при использовании внутри jsx js нужно использовать фигурные скобки, в данном примере мы обращаемся к объекту и достаем из него ключи, которые являются нашими стилями */}
         <img 
-          className="header__img"
+          className={classes.header__img}
           src="
           https://developers.sber.ru/studio/generated-images/cd/c7/b8/cdc7b8b687354a21ac8600aab0df2739_fuse_10004354_0.jpg"
         ></img>
